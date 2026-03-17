8702c3bb789990cfb387eb922cf9a73da29c0824
Adarsh Shukla <adarshshukla7655@gmail.com>
2026-03-17 14:58:00 +0530

    index,postController,utils

---
+++ index.js
@@ -0,0 +1,6 @@
+// const {genrateRandomNumber,celciusToFahrenheit} = require('./utils');
+
+// console.log(`Random Number : ${genrateRandomNumber()}`);
+// console.log(`Celcius to Fahrenheit : ${celciusToFahrenheit(0)}`)
+import { getposts } from "./postController.js"
+console.log(getposts())
